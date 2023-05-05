cdd8739fb5b9c59e4501f92035e43583197916bf
Niilo Kurki <niilo.kurki@helsinki.fi>
2023-05-05 22:50:42 +0300

    crsf token

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 import os
 
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__, instance_relative_config=True)
 app.secret_key = os.getenv('SECRET_KEY')
+csrf = CSRFProtect(app)
 
 import src.routes
